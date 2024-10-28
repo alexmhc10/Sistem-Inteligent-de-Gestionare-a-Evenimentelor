d3a5ffd1ffddec07294aa25e27fa87c780b35a4e
petricdarius <badlion30@gmail.com>
2024-10-28 13:18:56 +0200

    Mesaj despre modificările făcute

--- base/models.py
+++ base/models.py
@@ -1,13 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Location(models.Model):
+
+#Ne mai trebuie sau nu un class aici?
+
+class Type(models.Model):
     name = models.CharField(max_length=100)
-    #owner = 
-    description= models.TextField(null=True, blank=True)
-    #available_events =
-    #reviewers = 
-    gallery = models.ImageField(upload_to='images/', null=True, blank=True)
-    location= models.TextField(max_length=100)
 
     def __str__(self):
         return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    gallery = models.ImageField(upload_to='images/', null=True, blank=True)
+    location = models.TextField(max_length=100)
+    types = models.ManyToManyField(Type, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    comment = models.TextField()
+    stars = models.IntegerField(choices=[(i, f"{i} stele") for i in range(1, 6)], default=5)
+
+    def __str__(self):
+        return self.comment[0:20]
+    
