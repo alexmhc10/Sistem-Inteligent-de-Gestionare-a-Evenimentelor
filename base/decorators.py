be0f931d653f90560b7e64a4b7f18484acc8e8ab
HolczliAndrei <holczli_andrei@yahoo.com>
2025-01-15 13:15:44 +0200

    Adauagre profil invitat

--- base/decorators.py
+++ base/decorators.py
@@ -8,7 +8,7 @@ def user_is_organizer(view_func):
             if request.user.profile.user_type == 'organizer':  
                 return view_func(request, *args, **kwargs)
         messages.error(request, "Acces interzis: Trebuie să fii organizator pentru a accesa această pagină.")
-        return redirect(request.META.get('HTTP_REFERER', ''))
+        return redirect(request.META.get('HTTP_REFERER', '/'))
     return wrapper
 
 def user_is_staff(view_func):
@@ -17,7 +17,7 @@ def user_is_staff(view_func):
             if request.user.profile.user_type == 'staff':
                 return view_func(request, *args, **kwargs)
         messages.error(request, "Acces interzis: Trebuie să fii staff pentru a accesa această pagină.")
-        return redirect(request.META.get('HTTP_REFERER', ''))
+        return redirect(request.META.get('HTTP_REFERER', '/'))
     return wrapper
 
 def user_is_guest(view_func):
@@ -26,5 +26,5 @@ def user_is_guest(view_func):
             if request.user.profile.user_type == 'guest':
                 return view_func(request, *args, **kwargs)
         messages.error(request, "Acces interzis: Trebuie să fii invitat pentru a accesa această pagină.")
-        return redirect(request.META.get('HTTP_REFERER', ''))
+        return redirect(request.META.get('HTTP_REFERER', '/'))
     return wrapper
