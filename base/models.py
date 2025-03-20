d008374d5d6502d3827de41d800f9083b54c5c73
HolczliAndrei <116083078+HolczliAndrei@users.noreply.github.com>
2025-03-20 21:19:40 +0200

    Update models.py
    Titlu Guests

--- base/models.py
+++ base/models.py
@@ -150,6 +150,8 @@ class Guests(models.Model):
     vegan = models.BooleanField(default=False)
     allergens = models.JSONField(default=list, blank=True, null=True)
     state = models.BooleanField(default=False)
+    def __str__(self):
+        return self.profile.username
 
   
     
